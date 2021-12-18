6578a74da222a1402c97ca26a3e280aca50241ab
gael beard <gaelgael5@gmail.com>
2021-12-18 15:07:38 +0100

    initialize the solution

---
+++ Src/Black.Beard.CommandLine/CommandLines/Parameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bb.CommandLines
+{
+
+    public class Parameters
+    {
+
+        public string Token { get; set; }
+
+        public string ServerUrl { get; set; }
+
+        public string WorkingGroup { get; set; }
+
+        public DateTime TokenExpiration { get; set; }
+
+    }
+
+}
