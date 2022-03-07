a39157c0880ceed0f430147cb7ed75732563eeaf
gael beard <gaelgael5@gmail.com>
2022-03-07 07:56:24 +0100

    Add progress bar

---
+++ Src/Black.Beard.CommandLine/CommandLines/Outs/Output.ProgressBar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Bb.CommandLines.Outs
+{
+
+    public static partial class Output
+    {
+
+        public static void PrepareProgressBar(decimal completed, int maxChar = 0)
+        {
+
+            if (maxChar == 0)
+                maxChar = Console.WindowWidth;
+
+            var l1 = "0".PadRight(maxChar - 4, ' ') + "100";
+            Output.WriteLineStandard(l1);
+
+            var o = (int)(((maxChar - 1) / 2));
+            var l2 = "|".PadRight(o, ' ') + "|".PadRight(o, ' ') + "|";
+            Output.WriteLineStandard(l2);
+
+        }
+
+        public static void ShowProgressBar(decimal value, decimal completed, int maxChar = 0)
+        {
+
+            if (maxChar == 0)
+                maxChar = Console.WindowWidth;
+
+            int currentValue = (int)Math.Round(((value / completed * 100) / 100) * maxChar);
+
+            var l1 = "".PadRight(currentValue, CharsetProgressBarCompleted);
+            var l2 = l1.PadRight(maxChar - 1, CharsetProgressBar);
+
+            Output.WriteStandard(l2 + "\r");
+
+        }
+
+        public static char CharsetProgressBarCompleted { get; set; } = '█';
+
+
+        public static char CharsetProgressBar { get; set; } = '░';
+
+
+    }
+
+
+}
